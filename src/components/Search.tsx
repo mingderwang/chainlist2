20bc972c59be083cea4136c638d82ce4e3704a0c
Ming-der Wang <mingderwang@gmail.com>
2023-12-17 00:01:14 +0800

    v0.1.0-search

---
+++ src/components/Search.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useMemo } from 'react';
+import { fromFetch } from 'rxjs/fetch';
+import { of, mergeMap, catchError, startWith } from 'rxjs';
+import { $ } from 'react-rxjs-elements';
+import { timer } from 'rxjs';
+
+//export default function Search({ children, count: initialCount }){
+ // const [data, setData] = useState(null);
+ // const [count, setCount] = useState(initialCount);
+//  const subscription = fromFetch(`https://chainlist-api.muzamint.com/api/v1/chainlist/${count}`
+//        response.json().then(data => setData(JSON.stringify(data.meta)))
+  
+export default function Search
+  ({ children, count: initialCount }){
+    const [name, setName] = useState();
+
+  // we need useMemo to ensure stream$ persist
+  // between App re-renders
+  const stream$ = useMemo(() =>
+  fromFetch(`https://chainlist-api.muzamint.com/api/v1/chainlist/search/${name}`)
+      .pipe(
+        mergeMap(response => response.json().then(x => x.map((x:any) => ("👉" + x.meta.name))).catch(e => console.error(e))),
+        catchError(() => of('ERROR')),
+        startWith('loading...')
+      )
+  , [name]);
+
+//  return <div>Data: <$>{ stream$ }</$></div>
+
+
+  return <>
+      <div>
+      <form>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="search"
+        />
+        <p>{name}</p>
+      </form>
+    </div>
+
+  <div className="counter-message">{children}</div>
+
+  <div>Data: <$>{ stream$ }</$></div>
+  
+  </>
+}
+
